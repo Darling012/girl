81c5f511baa56c974a1672c72c52d78da360244b
Darling <782775618@qq.com>
2021-05-21 17:36:32 +0800

    add 日期param info 转换

---
+++ src/main/java/com/mvc/controller/TimeFormatController.java
@@ -0,0 +1,73 @@
+package com.mvc.controller;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @program: girl
+ * @version:
+ * @description: 入参出参中日期格式化
+ * @author: ling
+ * @create: 2021-05-21 16:19
+ **/
+@RestController
+@RequestMapping("time")
+public class TimeFormatController {
+    /**
+     * 不需要加convert等 此方式可成功
+     * @param time
+     * @return
+     */
+    @GetMapping("path/{time}")
+    public String testPathVariable(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime time) {
+        return time.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+
+    /**
+     * 不需要加convert等 此方式可成功
+     * @param time
+     * @return
+     */
+    @GetMapping("request-param")
+    public String testRequestParam(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime time) {
+        return time.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+
+    /**
+     * 需要增加Converter才可以
+     * DateHandlerConfig LocalDateTimeAdvice 都可以
+     * @param time
+     * @return
+     */
+    @GetMapping("request-param-pojo")
+    public TimeParam testRequestParamPojo(TimeParam time) {
+        return time;
+    }
+
+    @PostMapping("post/request-body-pojo")
+    public TimeParam testPostRequestBody(@RequestBody  TimeParam time) {
+        return time;
+    }
+
+
+    @Data
+    public static class TimeParam {
+        // 可解决
+        // @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        // @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        private LocalDateTime time;
+    }
+
+
+}
